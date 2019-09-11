00f573208d2d851aebfa8a3bec1931c18382d8a0
nongkrek <nongkrek@gmail.com>
2019-09-11 12:23:22 +0700

    Create Membuat file xShell.sh

---
+++ Crontab/Membuat file xShell.sh
@@ -0,0 +1,13 @@
+(1) Buat file xShell.sh dengan editor nano
+# nano xShell.sh
+
+(2) Isi file xShell.sh
+#!/bin/sh
+python /root/code_python/stomp-receive-multi.py
+
+(3) Simpan file xShell.sh
+(4) Ganti permission file xShell.sh
+# chmod 755 xShell.sh
+
+(5) Jalankan file xShell.sh
+# bash xShell.sh
